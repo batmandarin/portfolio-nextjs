4922a8ccc3e058803dedf922f1100747ce52ff5f
kuri-sun <soccer_haruki15@me.com>
2021-08-20 17:41:02 -0700

    timeline

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
 export const TimeLineData = [
-  { year: 2020, text: 'Started to study "Arduino"' },
+  { year: 2019, text: 'Started to study "Arduino"' },
+  { year: 2020, text: 'Finished studying "Arduino"' },
   { year: 2021, text: "Start to study Web and Mobile development" },
 ];
