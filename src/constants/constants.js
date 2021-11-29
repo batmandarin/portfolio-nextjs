5fe6cb7df434ab4e7085cca8225f464724785469
kuri-sun <soccer_haruki15@me.com>
2021-11-28 18:58:45 -0800

    configure constant value

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,4 +2,8 @@ export const TimeLineData = [
   { year: 2019, text: 'Started to study "Arduino"' },
   { year: 2020, text: 'Finished studying "Arduino"' },
   { year: 2021, text: "Start to study Web and Mobile development" },
+  {
+    year: 2022,
+    text: "I want to grab some exprience in Canada as much as possible!!",
+  },
 ];
