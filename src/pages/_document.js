7cf7c45c40a703c19ae5eefa844359cf513e1db9
kuri-sun <soccer_haruki15@me.com>
2021-12-17 23:15:12 -0800

    change sw

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { useEffect } from "react";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -30,25 +29,6 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    // to enable Service-Worker
-    useEffect(() => {
-      if ("serviceWorker" in navigator) {
-        window.addEventListener("load", function () {
-          navigator.serviceWorker.register("/sw.js").then(
-            function (registration) {
-              console.log(
-                "Service Worker registration successful with scope: ",
-                registration.scope
-              );
-            },
-            function (err) {
-              console.log("Service Worker registration failed: ", err);
-            }
-          );
-        });
-      }
-    }, []);
-
     return (
       <Html lang="en">
         <Head>
