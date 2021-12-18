f394db719a3b89f5a40930fa88569fc45d0eec7a
kuri-sun <soccer_haruki15@me.com>
2021-12-17 22:40:02 -0800

    sw.js chnaged

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,4 @@
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
+//public/sw.js
+self.addEventListener("install", function (event) {
+  console.log("From the Service Worker 🤙");
 });
