4867881964b02857378b85ed0fcdeb3e75ed97ea
kuri-sun <soccer_haruki15@me.com>
2021-12-17 23:27:21 -0800

    _app.js to index.js useEffect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,28 +3,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import { useEffect } from "react";
 
 function App({ Component, pageProps }) {
-  // to enable Service-Worker
+  // // to enable Service-Worker
   // useEffect(() => {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-      navigator.serviceWorker
-        .register("/sw.js", {
-          scope: "/",
-          registrationStrategy: "registerImmediately",
-        })
-        .then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-    });
-  }
+  // if ("serviceWorker" in navigator) {
+  //   window.addEventListener("load", function () {
+  //     navigator.serviceWorker
+  //       .register("/sw.js", {
+  //         scope: "/",
+  //         registrationStrategy: "registerImmediately",
+  //       })
+  //       .then(
+  //         function (registration) {
+  //           console.log(
+  //             "Service Worker registration successful with scope: ",
+  //             registration.scope
+  //           );
+  //         },
+  //         function (err) {
+  //           console.log("Service Worker registration failed: ", err);
+  //         }
+  //       );
+  //   });
+  // }
   // }, []);
 
   return (
