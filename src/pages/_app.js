5289ccea20b29e2cad23b4ba42e1b4d82e4eedf4
kuri-sun <soccer_haruki15@me.com>
2021-12-17 23:23:08 -0800

    _app.js change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,31 @@
 import Theme from "../styles/theme";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-export default App = ({ Component, pageProps }) => {
+function App({ Component, pageProps }) {
   // to enable Service-Worker
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker
-          .register("/sw.js", {
-            scope: "/",
-            registrationStrategy: "registerImmediately",
-          })
-          .then(
-            function (registration) {
-              console.log(
-                "Service Worker registration successful with scope: ",
-                registration.scope
-              );
-            },
-            function (err) {
-              console.log("Service Worker registration failed: ", err);
-            }
-          );
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/sw.js", {
+          scope: "/",
+          registrationStrategy: "registerImmediately",
+        })
+        .then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+    });
+  }
+  // }, []);
 
   return (
     <>
@@ -34,4 +34,6 @@ export default App = ({ Component, pageProps }) => {
       </Theme>
     </>
   );
-};
+}
+
+export default App;
