91352138d4fb86ac2e8042daee5f4ddf90c3458d
kuri-sun <soccer_haruki15@me.com>
2021-12-17 22:19:20 -0800

    changed _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,27 @@
 import Theme from "../styles/theme";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  // to enable Service-Worker
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js", { scope: "/" }).then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
+
   return (
     <>
       <Theme>
