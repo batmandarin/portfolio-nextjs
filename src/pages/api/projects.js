e0e85f3b26a523dc62919790865448a022507c1b
kuri-sun <soccer_haruki15@me.com>
2021-12-18 20:36:46 -0800

    change mongo uri

--- src/pages/api/projects.js
+++ src/pages/api/projects.js
@@ -2,8 +2,7 @@ import { MongoClient } from "mongodb";
 
 export default async function handler(req, res) {
   const client = await MongoClient.connect(
-    // `mongodb+srv://haruki:${process.env.MONGO_PASS}@cluster0.dqivi.mongodb.net/projects?retryWrites=true&w=majority`
-    "mongodb+srv://haruki:Haru1204@cluster0.dqivi.mongodb.net/projects?retryWrites=true&w=majority"
+    `mongodb+srv://haruki:${process.env.MONGO_PASS}@cluster0.dqivi.mongodb.net/projects?retryWrites=true&w=majority`
   );
   const db = client.db();
   const projectsCollection = db.collection("projects");
